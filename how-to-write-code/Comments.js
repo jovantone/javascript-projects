7b26019bc15399764ab962bb260a37eb4925b6e3
Tony Jovanovic <jovan.tone@gmail.com>
2023-09-12 10:08:23 -0500

    1

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -1,8 +1,9 @@
    // This demo shows off comments!
 
-   // console.log("This does not print.");
+   console.log("This\ndoes\nnot\nprint.");
 
-   console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
+   console.log("\tHello, World!"); // Comments do not have to start at the beginning of a line.
+   //but comments certainly can start at the beginning
 
    /* Here is how
    to have
@@ -10,3 +11,8 @@
    comments. */
 
    console.log("Comments make your code more readable by others.");
+
+   /*Lets'
+         go
+            multi
+               line */
