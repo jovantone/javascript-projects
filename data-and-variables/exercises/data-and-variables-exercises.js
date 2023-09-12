47aa5bab3877d364750fe224707163bbadc10b7c
jovantone <jovan.tone@gmail.com>
2023-09-11 19:44:30 -0500

    second update

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,6 +1,16 @@
 // Declare and assign the variables below
+let shuttleName = ('Determinaton');
+let shuttleSpeedMph = (17500);
+let distanceMarsKm = (225000000);
+let distanceMoonKm = (384400);
+const mpkm = (.621);
 
 // Use console.log to print the 'typeof' each variable. Print one item per line.
+console.log(typeof "Determination");
+console.log(typeof 17500);
+console.log(typeof 225000000);
+console.log(typeof 384400);
+console.log(typeof .621);
 
 // Calculate a space mission below
 
@@ -9,12 +19,4 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
-let shuttleName = 'Determinaton';
-console.log (shuttleName);
-
-let shuttleSpeedMph = (17500);
-console.log (shuttleSpeedMph);
-
-let distanceMoonKm = (225000000);
-
 
