a885ac598a98ec55f46bc19482e9832068d404f3
Tony Jovanovic <45926694+jovantone@users.noreply.github.com>
2023-09-14 22:41:29 -0500

    final submission

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,5 +1,5 @@
 // Declare and assign the variables below
-let shuttleName = 'Determinaton';
+let shuttleName = 'Determination';
 let shuttleSpeedMph = 17500;
 let distanceMarsKm = 225000000;
 let distanceMoonKm = 384400;
@@ -11,23 +11,27 @@ console.log(typeof shuttleSpeedMph);
 console.log(typeof distanceMarsKm);
 console.log(typeof distanceMoonKm);
 console.log(typeof mpkm);
+
 // Calculate a space mission below
 let milesToMars = distanceMarsKm * mpkm;
 let hoursToMars = milesToMars / shuttleSpeedMph;
 let daysToMars = hoursToMars / 24;
 
-
 // Print the results of the space mission calculations below
 console.log(milesToMars);
 console.log(hoursToMars);
 console.log(daysToMars);
+
+// I provided two solutions below
 console.log("Determination will take 332.67857142857144 days to reach Mars.");
+console.log(shuttleName + ' will take ' + daysToMars + ' days to reach Mars.');
 
 // Calculate a trip to the moon below
 
-
-
-
+let milesToMoon = distanceMoonKm * mpkm;
+let hoursToMoon = milesToMoon / shuttleSpeedMph;
+let daysToMoon = hoursToMoon / 24;
+console.log(daysToMoon);
 
 // Print the results of the trip to the moon below
-
+console.log("Determination will take 0.5683628571428571 days to reach the Moon.");
