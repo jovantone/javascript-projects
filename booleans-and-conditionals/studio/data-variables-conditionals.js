dae166005cf168cc755c12b64f1b70747d6331b0
Tony Jovanovic <45926694+jovantone@users.noreply.github.com>
2023-09-21 08:22:20 -0500

    studio 2

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -18,19 +18,49 @@ let preparedForLiftOff = 'true';
 
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
 
-if (astronautCount <= 7)
-
-
-
+if (astronautCount <= 7) {
+console.log('Astronaut Count: ' + astronautCount)  
+}
+else (console.log('Shutting down launch sequence.'));
 
 // add logic below to verify all astronauts are ready
 
+if (astronautStatus === 'ready') {
+console.log('Astronaut Status: ' + astronautStatus)  
+}
+else (console.log('Shutting down launch sequence.'));
+
 // add logic below to verify the total mass does not exceed the maximum limit of 850000
+
+if (totalMassKg < maximumMassLimit) {
+    console.log('Total mass: ' + totalMassKg)
+}
+else (console.log('Shutting down launch sequence.'));
 
 // add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
 
+if (fuelTempCelsius > minimumFuelTemp && fuelTempCelsius < maximumFuelTemp) {
+    console.log('Fuel Temppuature: ' + fuelTempCelsius)
+}
+else (console.log('Shutting down launch sequence.'));
+
 // add logic below to verify the fuel level is at 100%
+
+if (fuelLevel === '100 %') {
+    console.log('Fuel Level: ' + fuelLevel)
+}
+else (console.log('Shutting down launch sequence.'));
 
 // add logic below to verify the weather status is clear
 
+if (weatherStatus === 'clear') {
+    console.log('Weather Status: ' + weatherStatus)
+}
+else (console.log('Shutting down launch sequence.'));
+
 // Verify shuttle launch can proceed based on above conditions
+
+if (preparedForLiftOff === 'true') {
+    console.log('Have a safe trip astronauts!')
+}
+else (console.log('Shutting down launch sequence.'));
