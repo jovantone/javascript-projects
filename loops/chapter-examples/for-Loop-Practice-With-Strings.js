ab0d0a00ceb1e64610f1f0f306e68a70696d38d1
Tony Jovanovic <45926694+jovantone@users.noreply.github.com>
2023-09-25 16:29:47 -0500

    09/25/2023 update

--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,4 +1,23 @@
 // Create a string variable containing your name.
 
+let name = 'Tony Jovanovic';
 
 // Write a for loop that prints each character in your name on a different line.
+
+
+
+let phrase = "Chili Cook-off";
+
+for (let i = 0; i < phrase.length - 1; i = i + 3) {
+   console.log(phrase[i]);
+}
+
+let n = 10;
+let answer = 1;
+
+while (n > 0) {
+   answer = answer + n;
+   n = n + 1;
+}
+
+console.log(answer);
